23c7739c598201d0d89e320de6729a06cc9d2233
Amaia Castro <amaia@amaiac.net>
2017-06-19 12:34:52 +0200

    Export results to CSV

--- app/controllers/decidim/accountability/results_controller.rb
+++ app/controllers/decidim/accountability/results_controller.rb
@@ -11,6 +11,10 @@ module Decidim
       def home
       end
 
+      def csv
+        send_data CSVExporter.new(current_feature).export, filename: "results.csv", disposition: "attachment"
+      end
+
       private
 
       def results
